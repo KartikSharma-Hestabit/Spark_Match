cd940e915b6118ea7f684847b69377c97e7ba7d7
Hritwik Johri <58981259+hritwikjohri@users.noreply.github.com>
2025-03-21 16:38:35 +0530

    Clean up

--- app/src/main/java/com/hestabit/sparkmatch/screens/profile/Stories.kt
+++ app/src/main/java/com/hestabit/sparkmatch/screens/profile/Stories.kt
@@ -2,16 +2,10 @@ package com.hestabit.sparkmatch.screens.profile
 
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
-import com.hestabit.sparkmatch.common.HorizontalPagerLoopingIndicatorSample
-import com.hestabit.sparkmatch.common.HorizontalPagerWithGateTransition
 
 @Composable
 fun Stories(navController: NavController){
 
-//    HorizontalPagerWithGateTransition()
 
-//    HorizontalPagerLoopingIndicatorSample()
-
-    Gallery(navController)
 
 }
