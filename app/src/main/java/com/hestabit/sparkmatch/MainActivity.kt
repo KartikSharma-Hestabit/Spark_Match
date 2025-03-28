9031f61042b6c34c81154323fd3d99879115f765
Hritwik Johri <58981259+hritwikjohri@users.noreply.github.com>
2025-03-28 18:03:54 +0530

    Auth Navigation Setup and structure fixed

--- app/src/main/java/com/hestabit/sparkmatch/MainActivity.kt
+++ app/src/main/java/com/hestabit/sparkmatch/MainActivity.kt
@@ -5,14 +5,8 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
-import com.hestabit.sparkmatch.common.UpdateStatusBarColor
-import com.hestabit.sparkmatch.router.MainNavigator.InitMainNavigator
-import com.hestabit.sparkmatch.router.Routes
-import com.hestabit.sparkmatch.router.authNavHost.AuthNavigator
-import com.hestabit.sparkmatch.router.AuthRoute
 import com.hestabit.sparkmatch.screens.auth.AuthScreen
 import com.hestabit.sparkmatch.ui.theme.SparkMatchTheme
-import com.hestabit.sparkmatch.ui.theme.White
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
